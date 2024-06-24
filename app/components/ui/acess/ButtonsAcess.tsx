b6d88dd25f356437b07c9004468b16524bfa93eb
Jessica Aguiar <jessicaag.rs@gmail.com>
2024-06-24 11:14:45 -0300

    feat: adding homepage

---
+++ app/components/ui/acess/ButtonsAcess.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { ButtonsContainer } from "./ButtonAcess.styles";
+
+export const ButtonsAcess = () => {
+    return (
+        <ButtonsContainer>
+            <button>Cliente</button>
+            <button>Lojista</button>
+        </ButtonsContainer>
+    );
+};
---
+++ app/components/ui/acess/Logo.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Image from "next/image";
+import { ButtonsAcess } from "./ButtonsAcess";
+import { DataContainer, LogoContainer, LogoPhrase, LogoTitle } from "./Logo.styles";
+
+export default function Logo() {
+    return (
+        <LogoContainer>
+            <DataContainer>
+                <Image src='/logo-acess.png' width={420} height={350} alt="logo delivery app" />
+                <LogoTitle>Delivery APP</LogoTitle>
+                <LogoPhrase>Faça seus pedidos favoritos aqui! Conectando clientes a vários parceiros</LogoPhrase>
+            </DataContainer>
+            <ButtonsAcess />
+        </LogoContainer>
+    );
+};
